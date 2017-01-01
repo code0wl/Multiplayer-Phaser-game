845a7d0c7afcbcf0e4bb91569d24fd23c6d1d925
Oz <o.lodriguez@gmail.com>
2017-01-01 12:15:58 +0100

    merge stream

--- src/chapter-two/stop-watch.ts
+++ src/chapter-two/stop-watch.ts
@@ -4,16 +4,29 @@ export module StopWatch {
 
     const startButton = document.querySelector('.start');
     const stopButton = document.querySelector('.stop');
-
+    const resetButton = document.querySelector('.resetButton');
+    const startNumber: any = 0;
     const stop$ = Observable.fromEvent(stopButton, 'click');
-    const start$ = Observable.fromEvent(startButton, 'click')
+    const start$ = Observable.fromEvent(startButton, 'click');
+    const reset$ = Observable.fromEvent(resetButton, 'click');
+
     const interval$ = Observable.interval(1000);
 
     const stopInterval$ = interval$
-        .do(() => console.log('what am i?'))
         .takeUntil(stop$)
 
+    const inc = (acc, cur) => acc + 1;
+
+    const reset = acc => startNumber;
+
+    const timerChannel$ = Observable.merge(
+        stopInterval$.mapTo(inc),
+        reset$.mapTo(reset)
+    );
+
     start$
-        .switchMapTo(stopInterval$)
+        .switchMapTo(timerChannel$)
+        .startWith(startNumber)
+        .scan((acc, cur) => cur(acc))
         .subscribe(x => console.log(x));
 }
