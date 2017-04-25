04b60c75e35bbd6ed947ebef51d0cf6465073575
Oz <o.lodriguez@gmail.com>
2017-04-25 08:36:43 +0200

    unstable but cleaner

--- src/props/weapon/projectile.class.ts
+++ src/props/weapon/projectile.class.ts
@@ -3,12 +3,12 @@ declare const Phaser;
 export class Projectile {
     public game: any;
     public player: any;
-    public weapon: any = {};
+    public type: any;
 
     public constructor(player, game) {
         this.player = player;
         this.game = game;
-        this.weapon.bulletKillType = Phaser.Weapon.KILL_WORLD_BOUNDS;
+        this.type.bulletKillType = Phaser.Weapon.KILL_WORLD_BOUNDS;
     };
 
     public fire(): void {
