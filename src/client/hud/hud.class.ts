530d974f5159825b317af4d154caa442146aef6c
Oz <o.lodriguez@gmail.com>
2017-05-18 12:43:29 +0200

    max length name

--- src/client/hud/hud.class.ts
+++ src/client/hud/hud.class.ts
@@ -1,7 +1,6 @@
 export class Hud {
     static view(game, player): void {
-        const name = game.add.text(0, 30, player.name, {
-            align: "center",
+        const name = game.add.text(0, 10, player.name.substring(0, 6), {
             font: "10px Arial",
             fill: "#ffffff"
         });
--- src/client/props/powers/explosion/explosion.class.ts
+++ src/client/props/powers/explosion/explosion.class.ts
@@ -5,10 +5,10 @@ export class Explode  {
     constructor(gameInstance, projectile) {
         this.explosions = gameInstance.add.group();
         this.explosions.getFirstExists(false);
-        this.explosions.createMultiple(5, 'kaboom');
-        
+        this.explosions.createMultiple(1, 'kaboom');
+
         const explosion = this.explosions.getFirstExists(false);
-        explosion.reset(projectile.body.x + -20, projectile.body.y - 20);
-        explosion.play('kaboom', 5, false, true);
+        explosion.reset(projectile.body.x + -20, projectile.body.y - 30);
+        explosion.play('kaboom', 10, false, true);
     }
 }
