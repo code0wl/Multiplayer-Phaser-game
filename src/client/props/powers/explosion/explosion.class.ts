8131aa8b9ff136e352d71b44f46f4b67010cc7dd
Oz <o.lodriguez@gmail.com>
2017-05-20 12:35:15 +0200

    explosions revert

--- src/client/props/powers/explosion/explosion.class.ts
+++ src/client/props/powers/explosion/explosion.class.ts
@@ -4,11 +4,11 @@ export class Explode {
     constructor(gameInstance, projectile) {
         this.explosions = gameInstance.add.group();
         this.explosions.getFirstExists(false);
-        this.explosions.createMultiple(1, '');
+        this.explosions.createMultiple(1, 'kaboom');
 
         const explosion = this.explosions.getFirstExists(false);
         explosion.reset(projectile.body.x + -20, projectile.body.y - 30);
-        explosion.play('kaboom', [1, 2], 50);
+        explosion.play('kaboom', 50, false, true);
 
         setTimeout(() => {
             explosion.kill();
