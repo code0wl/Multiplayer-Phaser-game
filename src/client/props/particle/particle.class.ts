9c3ee27072872804a56f462e434187ae13ea207d
Oz <o.lodriguez@gmail.com>
2017-06-02 09:32:13 +0200

    refactored usage of particle

--- src/client/props/particle/particle.class.ts
+++ src/client/props/particle/particle.class.ts
@@ -2,10 +2,10 @@ export class Particle {
 
     private particle: Phaser.Sprite;
 
-    constructor(gameInstance: Phaser.Game, projectile: Phaser.Sprite) {
+    constructor(gameInstance: Phaser.Game, sprite: Phaser.Sprite) {
         this.particle = gameInstance.add.sprite(64, 64, 'dust');
         this.particle.animations.add('dust');
-        this.particle.reset(projectile.body.x + -20, projectile.body.y - 30);
+        this.particle.reset(sprite.body.x + -20, sprite.body.y - 30);
         this.particle.animations.play('dust', 16, false);
         setTimeout(() => {
             this.particle.kill();
