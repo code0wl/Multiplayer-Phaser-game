398a86ceb6b0771a6a051ae35576fd5e32343327
Oz <o.lodriguez@gmail.com>
2017-06-09 16:13:41 +0200

    specs coming soon

--- src/client/actors/player/player.class.spec.ts
+++ src/client/actors/player/player.class.spec.ts
@@ -1,10 +1,10 @@
-import {Player} from "./player.class";
+import {Player} from './player.class';
 
-describe("SpaceShip", () => {
+describe('SpaceShip', () => {
 
-	let player: Player;
+    let player: Player;
 
-	it("should have an id when it is created", () => {
-		expect(Player).toBeTruthy;
-	});
+    it('should have an id when it is created', () => {
+        expect(Player).toBeTruthy;
+    });
 });
