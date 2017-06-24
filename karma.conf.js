0badb6ec95fb48710abd9928767a16a71c50a9f5
Oz <o.lodriguez@gmail.com>
2017-06-24 19:47:56 +0200

    phantom

--- karma.conf.js
+++ karma.conf.js
@@ -9,7 +9,7 @@ module.exports = function (config) {
         colors: true,
         logLevel: config.LOG_INFO,
         autoWatch: true,
-        browsers: ['Chrome'],
+        browsers: ['PhantomJS'],
         phantomjsLauncher: {
             exitOnResourceError: true
         },
